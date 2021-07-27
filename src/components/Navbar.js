ea080560bf22fdf57bf53d9500a1f29c49a745dd
nithinalva <nsk254@gmail.com>
2021-07-27 23:53:35 +0530

    initial-commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,28 @@ import {Navbar, NavbarBrand, NavItem, NavLink,Nav,Button, NavbarText, Container}
 import {GoMarkGithub} from 'react-icons/go'
 import {IoLogoNpm} from 'react-icons/io'
 import {RiNpmjsLine}  from 'react-icons/ri'
+import { BrowserRouter as Router,Link } from 'react-router-dom'
 
 const NavbarC = () => {
     return (
         <div>
            
                 <Navbar className="navbar" expand="md">
+                    <Router>
                 <NavbarBrand>
                     <h1 style={{color:"#fff",padding:"2px"}} className="ml-2"><span><SiReact/></span>React-PexelStore</h1>
                 </NavbarBrand>
                 
             <Nav className="" navbar>
                 <NavItem>
-                <Button className="my-button btn btn-outline-light m-2 ">Pexels.com</Button>
+                <Button className="my-button btn btn-outline-light m-2 "><Link to={{pathname:"https://www.pexels.com/"}} target="_blank"> Pexels.com</Link></Button>
                 </NavItem>
                 <NavItem>
-                <Button className="my-button btn btn-outline-light m-2"><RiNpmjsLine/> Faker.js</Button>
+                <Button className="my-button btn btn-outline-light m-2"><RiNpmjsLine/><Link to={{pathname:"https://www.npmjs.com/package/faker"}} target="_blank"> Faker.js</Link> </Button>
                 </NavItem>
                 <NavItem>
                    
-                   <Button className="my-button btn btn-outline-light m-2"><GoMarkGithub/> source-code</Button>
+                   <Button className="my-button btn btn-outline-light m-2"><GoMarkGithub/><Link to={{pathname:"https://github.com/nithinalva/ThePexelStore-ReactJS-ContextAPI-Bootstrap"}} target="_blank"> source-code</Link> </Button>
                 </NavItem>
                
             
@@ -34,13 +36,14 @@ const NavbarC = () => {
             </Nav>
             
             <Nav className="left-nav p-2">
-                <Button className="my-button">
+                <Button className="my-button  "><Link to={{pathname: "https://nithin-alva.netlify.app/"}} target="_blank"  >
                       Developed By Nithin Alva
+                       </Link>
                 </Button>
             </Nav>
               
 
-            
+            </Router>
                 </Navbar>
 
              
